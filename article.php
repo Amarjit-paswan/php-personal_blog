468d7c7cc90227148154b16085926e4117c7b815
Amarjit-paswan <Amarjit-paswan>
2025-07-30 20:17:25 +0530

    add frontend design

---
+++ article.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article | Personal Blog</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container py-4">
+        <div class="title my-3">
+            <h2 class="fw-bold text-warning">My First Article</h2>
+            <p class="fs-5 fw-bold text-secondary">7 August, 2020</p>
+        </div>
+
+        <div class="row">
+            <div class="col-8">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, sit aliquid laborum quia veniam in debitis. Nam repellendus, incidunt facere, quaerat dolorum tempora officia illo fugit voluptates expedita adipisci reiciendis! Ab sapiente earum laudantium molestias soluta similique adipisci, possimus a corrupti repellat dolore voluptates reprehenderit dicta. Voluptatibus provident possimus nisi est, itaque reprehenderit similique pariatur dignissimos quos, quam, nam quasi sint a molestias ipsa magnam accusamus eum autem officiis ut expedita eius dolorem doloribus. Porro ullam quas error, animi voluptates, expedita quidem esse autem, incidunt tempore quisquam. Explicabo, repellendus, ea ipsa quibusdam in necessitatibus, saepe non voluptate illo adipisci tempora.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Personal Blog</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="title my-3">
+            <h1>Personal Blog</h1>
+        </div>
+
+        <div class="row">
+            <ul class="d-flex flex-column gap-3"> 
+                <li><a href="">
+                        <div class="d-flex w-100 justify-content-between align-items-center">
+                            <h4>My first article</h4>
+                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                        </div>
+                    </a>
+                </li>
+                <li><a href="">
+                        <div class="d-flex w-100 justify-content-between align-items-center">
+                            <h4>My first article</h4>
+                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                        </div>
+                    </a>
+                </li>
+                <li><a href="">
+                        <div class="d-flex w-100 justify-content-between align-items-center">
+                            <h4>My first article</h4>
+                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                        </div>
+                    </a>
+                </li>
+                <li><a href="">
+                        <div class="d-flex w-100 justify-content-between align-items-center">
+                            <h4>My first article</h4>
+                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                        </div>
+                    </a>
+                </li>
+                <li><a href="">
+                        <div class="d-flex w-100 justify-content-between align-items-center">
+                            <h4>My first article</h4>
+                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                        </div>
+                    </a>
+                </li>
+   
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
