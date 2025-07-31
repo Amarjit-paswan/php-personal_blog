bc38273ccec453dc48af53c860663a32ff39cf89
Amarjit-paswan <Amarjit-paswan>
2025-07-31 21:40:14 +0530

    add admin dashobard frontend

---
+++ admin/addBlog.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Blog | Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container ">
+        <div class="tittle my-3 py-3 border-bottom">
+            <h2>Add Personal Blog</h2>
+        </div>
+
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="" class="form-label"><b>Article Title</b></label>
+                <input type="text" name="" id="" class="form-control" placeholder="Enter Article Title"> 
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label"><b>Publishing Title</b></label>
+                <input type="text" name="" id="" class="form-control" placeholder="Enter Publishing Title">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label"><b>Content</b></label>
+                <textarea name="" id="" class="form-control" placeholder="Enter Content" rows="10"></textarea>
+            </div>
+
+            <div class="d-grid">
+                <button class="btn btn-warning">Add</button>
+            </div>
+
+
+
+            <div class="mb-3">
+                <label for="" class="form-label"></label>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+ 
+</head>
+<body>
+    <div class="container">
+        <div class="tittle my-3 py-4 border-bottom d-flex justify-content-between align-items-center">
+            <h3>Personal Blog | Dashboard</h3>
+
+            <button class="btn btn-warning">Add Blog</button>
+        </div>
+
+        <div class="row">
+            <table class="table align-middle text-center">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Blog Name</th>
+                        <th class="">Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Article No.</td>
+                        <td>
+                            <div class="d-flex justify-content-center gap-3">
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Article No.</td>
+                        <td>
+                            <div class="d-flex justify-content-center gap-3">
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Article No.</td>
+                        <td>
+                            <div class="d-flex justify-content-center gap-3">
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Article No.</td>
+                        <td>
+                            <div class="d-flex justify-content-center gap-3">
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Article No.</td>
+                        <td>
+                            <div class="d-flex justify-content-center gap-3">
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+              
+            </table>
+        </div>
+    </div>
+</body>
+</html>
