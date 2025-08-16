17560d97f81e1c4c9b1eeb189f5ba5726af4114d
Amarjit-paswan <amarjitp594@gmail.com>
2025-08-16 22:23:58 +0530

    add index

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php 
 
 // Include authentication file
-// require_once(dirname(__DIR__) .'/admin/conn/auth.php');
+require_once(__DIR__ .'/admin/conn/auth.php');
 
 // Load header file
-require_once('admin/common/header.php');
+require_once(__DIR__.'/admin/common/header.php');
+
+//Assign file
+$file_path = 'data/blogs.json';
+
+//json file store
+$json =[];
+
+//Check if file exist before reading
+    if(file_exists($file_path)){
+       
+        $blogs_data = file_get_contents($file_path);
+        if(!empty($blogs_data)){
+            $decoded = json_decode($blogs_data,true);
+
+            
+        }
+    }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | Personal Blog</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-</head>
+
 <body>
     <div class="container">
         <div class="title my-3">
@@ -25,41 +34,16 @@ require_once('admin/common/header.php');
 
         <div class="row">
             <ul class="d-flex flex-column gap-3"> 
+                <?php foreach($decoded as $blog): ?>
                 <li><a href="">
                         <div class="d-flex w-100 justify-content-between align-items-center">
-                            <h4>My first article</h4>
-                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
+                            <h4><?= htmlspecialchars($blog['article_title'],ENT_QUOTES,'UTF-8') ?></h4>
+                            <p class="m-0 fs-5 text-secondary"><?= htmlspecialchars($blog['publish_date'],ENT_QUOTES,'UTF-8') ?></p>
                         </div>
                     </a>
                 </li>
-                <li><a href="">
-                        <div class="d-flex w-100 justify-content-between align-items-center">
-                            <h4>My first article</h4>
-                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
-                        </div>
-                    </a>
-                </li>
-                <li><a href="">
-                        <div class="d-flex w-100 justify-content-between align-items-center">
-                            <h4>My first article</h4>
-                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
-                        </div>
-                    </a>
-                </li>
-                <li><a href="">
-                        <div class="d-flex w-100 justify-content-between align-items-center">
-                            <h4>My first article</h4>
-                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
-                        </div>
-                    </a>
-                </li>
-                <li><a href="">
-                        <div class="d-flex w-100 justify-content-between align-items-center">
-                            <h4>My first article</h4>
-                            <p class="m-0 fs-5 text-secondary"> 7 August 2025</p>
-                        </div>
-                    </a>
-                </li>
+                <?php endforeach; ?>
+              
    
             </ul>
         </div>
